0678c729f424996bcc2fa069a623f3894bae940a
Kyle McGregor <kyle.mcgregor@hotmail.com>
2023-12-20 10:24:32 -0500

    adding data loaders

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
+import './style.css';
+import Data from '.data.csv';
+
+function loadData(){
+    console.log(Data);
+}
 
 function component(){
     const element = document.createElement('div'); 
 
     element.innerHTML = _.join(['Hello', 'webpack', 'How are you?'], ' '); 
+    element.classList.add('hello');
 
     return element;
 }
